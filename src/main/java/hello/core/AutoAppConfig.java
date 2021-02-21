16abcf6cdc187b6478141c69ab8a547ed9bd9d86
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-02-21 14:44:06 +0900

    [feat] AutoAppConfig 추가
    - AutoAppConfig 추가

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        // 이전 예제에 있는 @Configuration들과의 출동을 막기 위해서 설정함
+        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository () {
+//        return new MemoryMemberRepository();
+//    }
+}
+
+
